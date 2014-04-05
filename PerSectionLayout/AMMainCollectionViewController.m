5e586a76afae223d2374ddd5f05e292453037658
Anthony Mittaz <anthony.mittaz@rea-group.com>
2014-04-05 18:49:24 +1100

    started using section provider in main collection view controller

--- PerSectionLayout/AMMainCollectionViewController.m
+++ PerSectionLayout/AMMainCollectionViewController.m
@@ -3,11 +3,53 @@
 //
 
 #import "AMMainCollectionViewController.h"
+#import "AMSectionsProvider.h"
 
 @interface AMMainCollectionViewController ()
-
+@property (nonatomic, strong) AMSectionsProvider *sectionsProvider;
 @end
 
 @implementation AMMainCollectionViewController
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self)
+    {
+        self.sectionsProvider = [[AMSectionsProvider alloc] init];
+    }
+    
+    return self;
+}
+
+#pragma mark - View Lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [self.sectionsProvider registerCustomElementsForCollectionView:self.collectionView];
+}
+
+#pragma mark - UICollectionViewDataSource
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return (NSInteger)self.sectionsProvider.sectionControllersCount;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    id<AMSectionController> sectionController = [self.sectionsProvider controllerForSection:section];
+    
+    return [sectionController collectionView:collectionView numberOfItemsInSection:section];
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    id<AMSectionController> sectionController = [self.sectionsProvider controllerForSection:indexPath.section];
+    
+    return [sectionController collectionView:collectionView cellForItemAtIndexPath:indexPath];
+}
 
 @end
