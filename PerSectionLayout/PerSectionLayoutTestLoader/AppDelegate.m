eaf44a8dd7b634413d39f312c458aaa7401f5771
Anthony Mittaz <anthony.mittaz@rea-group.com>
2014-04-15 17:52:53 +1000

    added test loader + removed Sample app test target

---
+++ PerSectionLayout/PerSectionLayoutTestLoader/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  Copyright (c) 2014 Dblechoc. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+@end
---
+++ PerSectionLayout/PerSectionLayoutTestLoader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PerSectionLayoutTestLoader
+//
+//  Created by Anthony Mittaz on 15/04/2014.
+//  Copyright (c) 2014 Dblechoc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
